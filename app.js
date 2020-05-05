d0d56f802cf5c718cb2ad70e885b4aae979b132a
jose blois <joseblois3@gmail.com>
2020-05-05 04:46:10 -0300

    fixed some variables errors

--- app.js
+++ app.js
@@ -9,13 +9,15 @@
     var lastPress=null;
     var mousex=0,mousey=0;
     var score=0,counter=0;
+    var me = 0;
     var bgColor='#000';
     var players=[];
     var player=new Circle(0,0,5);
     var target=new Circle(100,100,10);
     var spritesheet=new Image();
     spritesheet.src='assets/targetshoot.png';
-
+    var colors = ['#0f0', '#00f', '#ff0', '#f00'];
+    
     function init(){
         canvas=document.getElementById('canvas');
         ctx=canvas.getContext('2d');
@@ -27,10 +29,6 @@
         run();
     }
 
-    function random(max){
-        return ~~(Math.random()*max);
-    }
-
     function run(){
         requestAnimationFrame(run);
             
@@ -39,52 +37,8 @@
         if(deltaTime>1000)deltaTime=0;
         time=now;
         
-        act(deltaTime);
+        // act(deltaTime);
         paint(ctx);
-    }
-
-    function act(deltaTime){
-        player.x=mousex;
-        player.y=mousey;
-
-        if(player.x<0)
-            player.x=0;
-        if(player.x>canvas.width)
-            player.x=canvas.width;
-        if(player.y<0)
-            player.y=0;
-        if(player.y>canvas.height)
-            player.y=canvas.height;
-        
-        if(moving){
-            socket.emit('mySight', { x : player.x, y : player.y});
-            moving=false;
-        }
-
-        counter-=deltaTime;
-        if(!pause){
-            if(lastPress==1){
-                bgColor='#333';
-                if(player.distance(target)<0){
-                    score++;
-                    target.x=random(canvas.width/10-1)*10+target.radius;
-                    target.y=random(canvas.height/10-1)*10+target.radius;
-                }
-            }
-            else{
-                bgColor='#000';
-            }
-
-            if(counter<1){
-                pause=true;
-            }
-        }
-        else if(lastPress==1&&counter<-1000){
-            pause=false;
-            counter=10000;
-            score=0;
-        }
-        lastPress=null;
     }
 
     function paint(ctx){
@@ -96,25 +50,33 @@
         ctx.strokeStyle='#0f0';
         //player.stroke(ctx);
         for(var i=0,l=players.length;i<l;i++){
-            if(players[i]!==null)
+            if(players[i]!==null){
                 players[i].drawImageArea(ctx,spritesheet,10*(i%4),20,10,10);
+                ctx.fillStyle=colors[i%4];
+                ctx.fillText('Score'+ players[i].score,0, 10 + i *10);
+            }
         }
 
         ctx.fillStyle='#fff';
         //ctx.fillText('Distance: '+player.distance(target).toFixed(1),0,10);
-        ctx.fillText('Score: '+score,0,10);
+        // ctx.fillText('Score: '+score,0,10);
         if(counter>0)
             ctx.fillText('Time: '+(counter/1000).toFixed(1),250,10);
         else
             ctx.fillText('Time: 0.0',250,10);
         if(pause){
-            ctx.fillText('Score: '+score,120,100);
+            ctx.fillText('Your Score: '+players[me].score,120,100);
             if(counter<-1000)
                 ctx.fillText('CLICK TO START',100,120);
         }
     }
     
     function enableSockets() {
+
+        socket.on('me',function (sight){
+            me = sight.id;
+        });
+
         socket.on('sight', function (sight) {
             if (sight.x === null && sight.y === null) {
                 players[sight.id] = null;
@@ -125,31 +87,50 @@
                 players[sight.id].y = sight.y;
             }
         });
+
+        socket.on('score', function(sight){
+            players[sight.id].score += sight.score;
+        })
+
+        socket.on('target',function (t){
+            target.x = t.x;
+            target.y = t.y;
+        })
     }
 
     function enableInputs(){
         document.addEventListener('mousemove',function(evt){
             mousex=evt.pageX-canvas.offsetLeft;
             mousey=evt.pageY-canvas.offsetTop;
-            moving=true;
+            emitSight(mousex,mousey,0)
         },false);
         canvas.addEventListener('mousedown',function(evt){
-            lastPress=evt.which;
+            // lastPress=evt.which;
+            emitSight(mousex,mousey,evt.which);
         },false);
+    }
+
+    function emitSight(x,y,lastPress){
+        if(x < 0 ){
+            x = 0;
+        }
+        if(x > canvas.width){
+            x = canvas.width
+        }
+        if(y > canvas.height){
+            y = canvas.height;
+        }
+        if(y < 0){
+            y = 0;
+        }
+        socket.emit('mySight', {x : x , y : y , lastPress : lastPress});
     }
 
     function Circle(x,y,radius){
         this.x=(x==null)?0:x;
         this.y=(y==null)?0:y;
         this.radius=(radius==null)?0:radius;
-    }
-
-    Circle.prototype.distance=function(circle){
-        if(circle!=null){
-            var dx=this.x-circle.x;
-            var dy=this.y-circle.y;
-            return (Math.sqrt(dx*dx+dy*dy)-(this.radius+circle.radius));
-        }
+        this.score = 0;
     }
 
     Circle.prototype.stroke=function(ctx){
--- server.js
+++ server.js
@@ -5,12 +5,37 @@
         server = null,
         io = null,
         nSight = 0;
+    var gameEnd = 0,
+        canvasWidth = 300,
+        canvasHeight = 200,
+        players = [],
+        target = null;
+
+    function Circle (x , y , radius){
+        this.x = (x === undefined)? 0 : x;
+        this.y = (y === undefined)? 0 : y;
+        this.radius = (radius === undefined)? 0 : radius;
+    }
+
+    Circle.prototype ={
+        constructor : Circle,
+
+        distance : function(circle){
+            if(circle !== undefined){
+            var dx=this.x-circle.x;
+            var dy=this.y-circle.y;
+            return (Math.sqrt(dx*dx+dy*dy)-(this.radius+circle.radius));
+            }
+        }
+    };
 
     function MyServer(request, response) {
         response.writeHead(200, {'Content-Type': 'text/html'});
         response.end('<h1>It\'s working!</h1>');
     }
     
+    target = new Circle(100, 100, 10);
+
     server = require('http').createServer(MyServer);
     server.listen(serverPort, function () {
         console.log('Server is listening on port ' + serverPort);
@@ -20,11 +45,18 @@
     io.sockets.on('connection', function (socket) {
         socket.playerId = nSight;
         nSight += 1;
+        players[socket.playerId] = new Circle(0, 0, 5);
+        socket.emit('me', {id : socket.playerId});
         io.sockets.emit('sight', {id: socket.playerId, x: 0, y: 0});
         console.log(socket.id + ' connected as player ' + socket.playerId);
 
         socket.on('mySight', function (sight) {
-            io.sockets.emit('sight', {id: socket.playerId, x: sight.x, y: sight.y});
+            players[socket.playerId].x = sight.x;
+            players[socket.playerId].y = sight.y;
+            if(sight.lastPress ===1){
+                act(socket.playerId)
+            }
+            io.sockets.emit('sight', {id: socket.playerId, x: sight.x, y: sight.y, lastPress : sight.lastPress});
         });
 
         socket.on('disconnect', function () {
@@ -32,4 +64,27 @@
             console.log('Player' + socket.playerId + ' disconnected.');
         });
     });
+
+    function random(max){
+        return ~~(Math.random()*max);
+    }
+    
+    function act(player){
+        var now  = Date.now();
+        if(gameEnd - now < -1000){
+            gameEnd = now + 10000;
+            io.sockets.emit('gameEnd', {time : gameEnd});
+            target.x = random((canvasWidth/10-1) * 10 +target.radius)
+            target.y = random((canvasHeight/10-1) * 10 +target.radius)
+            io.sockets.emit('target', {x: target.x, y: target.y});
+        } else if (gameEnd - now > 0){
+            if(players[player].distance(target)< 0){
+                io.sockets.emit('score', {id: player , score : 1});
+                target.x = random((canvasWidth/10-1) * 10 +target.radius)
+                target.y = random((canvasHeight/10-1) * 10 +target.radius)
+                io.sockets.emit('target', {x : target.x , y : target.y});
+            }
+        }
+    }
+
 }());
